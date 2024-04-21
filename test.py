5cf6f869b9e483943b0fe84030530e454bb42c53
Bodhi14 <bodhi140802@gmail.com>
2024-04-21 23:05:04 +0530

    Multiple Faces detection updated

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import csv
 import time
 from datetime import datetime
 
-def test(duration = 5):
+def test(duration=2):
     detector = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
     with open('data/roll_nos.pkl', 'rb') as f:
@@ -27,15 +27,24 @@ def test(duration = 5):
         if frame is None:
             break
         gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = detector.detectMultiScale(gray_scale, 1.3, 4) 
+        detected_faces = detector.detectMultiScale(gray_scale, 1.3, 4)
 
-        for (x, y, w, h) in faces:
+        detected_labels = []
+        detected_images = []
+
+        for (x, y, w, h) in detected_faces:
             cropped_image = frame[y:y+h, x:x+w, :]
             resized_image = cv2.resize(cropped_image, (50, 50)).flatten().reshape(1, -1)
-            output = knn.predict(resized_image)
+            detected_images.append(resized_image)
+
+        if detected_images:
+            detected_images = np.vstack(detected_images)
+            detected_labels = knn.predict(detected_images)
+
+        for (x, y, w, h), label in zip(detected_faces, detected_labels):
             current_time = time.strftime("%H:%M:%S")
             current_date = time.strftime("%d-%m-%Y")
-            student_label = str(output[0])
+            student_label = str(label)
 
             attendance_file = f"Records/{student_label}_Attendance.csv"
             is_exists = os.path.isfile(attendance_file)
@@ -46,9 +55,14 @@ def test(duration = 5):
                     wr.writerow(['Roll_No', 'Date', 'Time'])
                 wr.writerow([student_label, current_date, current_time])
 
+            cv2.putText(frame, student_label, (x, y-15), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (50, 50, 255), 4)
+
+        # cv2.imshow("frame", frame)
         k = cv2.waitKey(1) 
+
         if time.time() - start_time >= duration:
             break
         Video.release()
-
+        
     cv2.destroyAllWindows()
